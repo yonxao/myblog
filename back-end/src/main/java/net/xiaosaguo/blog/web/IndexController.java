843fd7a1cae73d55a5e8f922d8e60f5aa66fafa9
guoyx <guoyx@vip.qq.com>
2020-04-23 06:40:06 +0800

    add:将静态页面放入后端中，页面中一些静态资源无法访问，使用thymeleaf提供的静态资源引入机制，将静态资源使用thymeleaf引入一边

--- back-end/src/main/java/net/xiaosaguo/blog/web/IndexController.java
+++ back-end/src/main/java/net/xiaosaguo/blog/web/IndexController.java
@@ -1,6 +1,5 @@
 package net.xiaosaguo.blog.web;
 
-import net.xiaosaguo.blog.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -13,10 +12,16 @@ public class IndexController {
     @GetMapping("/")
     public String index() {
 //        int i = 9 / 0;
-        String blog = null;
-        if (blog == null) {
-            throw new NotFoundException("博客不存在");
-        }
+//        String blog = null;
+//        if (blog == null) {
+//            throw new NotFoundException("博客不存在");
+//        }
+        System.out.println("----------index----------");
         return "index";
+    }
+
+    @GetMapping("/blog")
+    public String blog() {
+        return "blog";
     }
 }
