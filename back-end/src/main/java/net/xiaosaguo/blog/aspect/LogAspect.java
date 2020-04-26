cfa700e5bd20368b246d20191585aa68fcdd5553
guoyx <guoyx@vip.qq.com>
2020-04-26 23:07:31 +0800

    upd:优化请求日志格式

--- back-end/src/main/java/net/xiaosaguo/blog/aspect/LogAspect.java
+++ back-end/src/main/java/net/xiaosaguo/blog/aspect/LogAspect.java
@@ -1,7 +1,6 @@
 package net.xiaosaguo.blog.aspect;
 
 import lombok.AllArgsConstructor;
-import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
@@ -10,12 +9,12 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
 
 /**
  * description: 使用aop记录请求日志
  *
  * @author xiaosaguo
- * @version 1 xiaosaguo 创建
  * @version 2 xiaosaguo 在日志对象中增加请求方式
  */
 @Aspect
@@ -29,36 +28,52 @@ public class LogAspect {
 
     @Before("log()")
     public void doBefore(JoinPoint joinPoint) {
-        log.info("--------------before--------------");
+        log.debug("------request-----before------start--------");
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        assert attributes != null;
         HttpServletRequest request = attributes.getRequest();
         String method = request.getMethod();
         String url = request.getRequestURL().toString();
         String ip = request.getRemoteAddr();
-        String classMethod = joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName();
+        String handler = joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName();
         Object[] args = joinPoint.getArgs();
-        RequestLog requestLog = new RequestLog(method, url, ip, classMethod, args);
-        log.info("Request : {}", requestLog);
+        RequestParam requestParam = new RequestParam(method, url, ip, handler, args);
+        log.info("\n{}", requestParam);
+        log.debug("------request-----before------end----------");
     }
 
     @After("log()")
     public void doAfter() {
-        log.info("--------------after--------------");
+        log.debug("------request-----after------start--------");
+        log.debug("------request-----after------end----------");
     }
 
     @AfterReturning(returning = "result", pointcut = "log()")
     public void doAfterReturn(Object result) {
-        log.info("Result : {}", result);
+        log.debug("------request-----result------start--------");
+        log.info("\nRequestResult : {}", result);
+        log.debug("------request-----result------end----------");
     }
 
     @AllArgsConstructor
-    @ToString
-    private class RequestLog {
+    private static class RequestParam {
         private String method;
         private String url;
         private String ip;
-        private String classMethod;
+        private String handler;
         private Object[] args;
+
+        @Override
+        public String toString() {
+            return "RequestParam : " +
+                    "\n{" +
+                    "\n\tmethod  = '" + method + "'," +
+                    "\n\turl     = '" + url + "'," +
+                    "\n\tip      = '" + ip + "'," +
+                    "\n\thandler = '" + handler + "'," +
+                    "\n\targs    = " + Arrays.toString(args) +
+                    "\n}";
+        }
     }
 
 }
