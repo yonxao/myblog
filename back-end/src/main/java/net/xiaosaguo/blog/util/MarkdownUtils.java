fb66a2b81a1fc45120fda87d5cecdf8c6dc35c2e
xiaosaguo <guoyx@vip.qq.com>
2020-05-20 04:12:39 +0800

    add : 引入 commonmark 及 MarkdownUtils 工具类

---
+++ back-end/src/main/java/net/xiaosaguo/blog/util/MarkdownUtils.java
@@ -0,0 +1,97 @@
+package net.xiaosaguo.blog.util;
+
+import org.commonmark.Extension;
+import org.commonmark.ext.gfm.tables.TableBlock;
+import org.commonmark.ext.gfm.tables.TablesExtension;
+import org.commonmark.ext.heading.anchor.HeadingAnchorExtension;
+import org.commonmark.node.Link;
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.AttributeProvider;
+import org.commonmark.renderer.html.AttributeProviderContext;
+import org.commonmark.renderer.html.AttributeProviderFactory;
+import org.commonmark.renderer.html.HtmlRenderer;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * description: 将 Markdown 文档转换为 HTML 文档的工具类
+ *
+ * @author xiaosaguo
+ * @date 2020/05/20
+ */
+public class MarkdownUtils {
+
+    /**
+     * Markdown 格式转换成 HTML格式
+     *
+     * @param markdown Markdown 格式的文档
+     * @return HTML格式的文档
+     */
+    public static String markdownToHtml(String markdown) {
+        Parser parser = Parser.builder().build();
+        Node document = parser.parse(markdown);
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        return renderer.render(document);
+    }
+
+    /**
+     * 增加扩展 [标题锚点，表格生成]
+     * Markdown 转换成 HTML
+     *
+     * @param markdown Markdown 格式的文档
+     * @return HTML格式的文档
+     */
+    public static String markdownToHtmlExtensions(String markdown) {
+        // h 标题生成 id
+        Set<Extension> headingAnchorExtensions = Collections.singleton(HeadingAnchorExtension.create());
+        // 转换 table 的 HTML
+        List<Extension> tableExtension = Collections.singletonList(TablesExtension.create());
+        Parser parser = Parser.builder()
+                .extensions(tableExtension).build();
+        Node document = parser.parse(markdown);
+        HtmlRenderer renderer = HtmlRenderer.builder()
+                .extensions(headingAnchorExtensions)
+                .extensions(tableExtension)
+                .attributeProviderFactory(new AttributeProviderFactory() {
+                    @Override
+                    public AttributeProvider create(AttributeProviderContext context) {
+                        return new CustomAttributeProvider();
+                    }
+                })
+                .build();
+        return renderer.render(document);
+    }
+
+
+    /**
+     * 处理标签的属性
+     */
+    static class CustomAttributeProvider implements AttributeProvider {
+        @Override
+        public void setAttributes(Node node, String tagName, Map<String, String> attributes) {
+            // 改变 a 标签的 target 属性为 _blank
+            if (node instanceof Link) {
+                attributes.put("target", "_blank");
+            }
+            if (node instanceof TableBlock) {
+                attributes.put("class", "ui celled table");
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        String table = "| hello | hi | 哈哈哈 |\n" +
+                "| ----- | ---- | ----- |\n" +
+                "| 斯维尔多 | 士大夫 | f啊 |\n" +
+                "| 阿什顿发 | 非固定杆 | 撒阿什顿发 |\n" +
+                "\n";
+        String a = "[imCoding 爱编程](http://www.lirenmi.cn)";
+        System.out.println(markdownToHtmlExtensions(a));
+        System.out.println(markdownToHtmlExtensions(table));
+    }
+}
